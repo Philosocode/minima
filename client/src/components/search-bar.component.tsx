6d905dd33be9041dc8ef883fe49e65da8a2b3596
Tam Le <philosocode@gmail.com>
2020-04-08 10:55:46 -0600

    Work on search bar and styles

--- client/src/components/search-bar.component.tsx
+++ client/src/components/search-bar.component.tsx
@@ -1,5 +1,23 @@
-import React, { FC } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-export const SearchBar: FC = () => (
-  <div className="c-search-bar"></div>
-)
+export const SearchBar: FC = () => { 
+  const [searchText, setSearchText] = useState("");
+
+  const handleChange = (ev: ChangeEvent<HTMLInputElement>) => {
+    setSearchText(ev.target.value);
+  }
+
+  return (
+    <div className="c-search-bar">
+      <input
+        type="text"
+        className="c-search-bar__input"
+        value={searchText}
+        onChange={handleChange}
+      />
+      <FontAwesomeIcon icon={faTimes} className="c-search-bar__clear" />
+    </div>
+  )
+ }
