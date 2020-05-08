62bf498520dc81838c81ff1f55377dbee3a46f22
Tam Le <philosocode@gmail.com>
2020-05-08 14:44:17 -0600

    Begin refactoring SASS. Refactor comment-thread component

--- client/src/components/comment-thread.component.tsx
+++ client/src/components/comment-thread.component.tsx
@@ -8,13 +8,18 @@ interface IProps {
   thread: ICommentThread;
 }
 
-export const CommentThread: FC<IProps> = ({ thread }) => { 
+export const CommentThread: FC<IProps> = ({ thread }) => {
   const { topLevelComment, totalReplyCount } = thread.snippet;
 
   return (
-    <div className="c-comment__thread">
+    <div className="o-container u-margin-bottom--large">
       <Comment comment={topLevelComment} type="comment" />
-      { totalReplyCount > 0 && <ReplyList topLevelCommentId={topLevelComment.id} totalReplyCount={totalReplyCount} /> }
+      {totalReplyCount > 0 && (
+        <ReplyList
+          topLevelCommentId={topLevelComment.id}
+          totalReplyCount={totalReplyCount}
+        />
+      )}
     </div>
-  )
- };
+  );
+};
