5192212df24e8ee1a03ac7563383d024a700967d
Tam Le <philosocode@gmail.com>
2020-05-11 20:15:48 -0600

    Make header fixed to top

--- client/src/components/header.component.tsx
+++ client/src/components/header.component.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { SearchBar } from "components/search-bar.component";
 
 export const Header: FC = () => (
-  <header className="o-grid__item--wide c-header__container">
+  <header className="c-header__container">
     <nav className="c-header__nav">
       <Link to="/" className="c-header__logo">minima</Link>
       <SearchBar />
