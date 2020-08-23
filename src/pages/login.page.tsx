30f602ccd637faa51256ba3c97033bd55e41afd8
Tam Le <philosocode@gmail.com>
2020-08-23 00:47:53 -0600

    Redirect from Login page if logged in

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -1,19 +1,27 @@
 import React, { FC } from "react";
+import { Redirect } from "react-router-dom";
 
+import { useAuth } from "hooks/use-auth.hook";
 import { LoginForm } from "components/form/login-form.component";
 
-export const LoginPage: FC = () => (
-  <div className="o-grid">
-    <div className="o-grid__item--wide">
-      <LoginForm />
-      {
-        process.env.REACT_APP_DEMO && (
-          <div className="c-demo" style={{ textAlign: "center", margin: "0 auto" }}>
+export const LoginPage: FC = () => {
+  const userId = useAuth();
+  if (userId) return <Redirect to="/" />;
+
+  return (
+    <div className="o-grid">
+      <div className="o-grid__item--wide">
+        <LoginForm />
+        {process.env.REACT_APP_DEMO && (
+          <div
+            className="c-demo"
+            style={{ textAlign: "center", margin: "0 auto" }}
+          >
             <h3>Email: test@test.com</h3>
             <h3>Password: password</h3>
-          </div> 
-        )
-      }
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
